ea7cf70c959bdcbcfb4ecccd4dfd52759cec369b
valebladi <bladinieresjusto@campus.tu-berlin.de>
2023-08-02 14:36:31 +0200

    built tables working, change - for uppercase

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -4,45 +4,45 @@ DROP TABLE IF EXISTS books_users;
 DROP TABLE IF EXISTS trade;
 
 CREATE TABLE users (
-    user_id INT NOT NULL AUTO_INCREMENT,
-    bond_holder VARCHAR(100) NOT NULL,
+    userId INT NOT NULL AUTO_INCREMENT,
+    bondHolder VARCHAR(100) NOT NULL,
     type VARCHAR(50) NOT NULL,
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (userId)
 );
 
 CREATE TABLE books (
-    book_id INT NOT NULL AUTO_INCREMENT,
-    book_name VARCHAR(100) NOT NULL,
+    bookId INT NOT NULL AUTO_INCREMENT,
+    bookName VARCHAR(100) NOT NULL,
     status VARCHAR(50) NOT NULL,
-    bond_maturity_date DATE,
-    issuer_name VARCHAR(200) NOT NULL,
+    bondMaturityDate DATE,
+    issuerName VARCHAR(200) NOT NULL,
     isin VARCHAR(100) NOT NULL,
-    face_value INT NOT NULL,
+    faceValue INT NOT NULL,
     cusip VARCHAR(100),
-    bond_currency VARCHAR(100) NOT NULL,
-    coupon_percent DOUBLE NOT NULL,
-    unit_price DOUBLE NOT NULL,
-    PRIMARY KEY (book_id)
+    bondCurrency VARCHAR(100) NOT NULL,
+    couponPercent DOUBLE NOT NULL,
+    unitPrice DOUBLE NOT NULL,
+    PRIMARY KEY (bookId)
 );
 
 CREATE TABLE books_users (
-    bond_holder VARCHAR(250) NOT NULL,
-    book_name VARCHAR(250) NOT NULL,
-    PRIMARY KEY (bond_holder, book_name)
+    bondHolder VARCHAR(250) NOT NULL,
+    bookName VARCHAR(250) NOT NULL,
+    PRIMARY KEY (bondHolder, bookName)
 );
 
 
 CREATE TABLE trade (
-    trade_id int NOT NULL AUTO_INCREMENT,
-    trade_type VARCHAR(250) NOT NULL,
-    book_name VARCHAR(100) NOT NULL,
+    tradeId int NOT NULL AUTO_INCREMENT,
+    tradeType VARCHAR(250) NOT NULL,
+    bookName VARCHAR(100) NOT NULL,
     isin VARCHAR(100) NOT NULL,
-    trade_currency VARCHAR(250) NOT NULL,
-    trade_date DATE NOT NULL,
-    trade_status VARCHAR(50) NOT NULL,
-    trade_settlement_date DATE NOT NULL,
+    tradeCurrency VARCHAR(250) NOT NULL,
+    tradeDate DATE NOT NULL,
+    tradeStatus VARCHAR(50) NOT NULL,
+    tradeSettlementDate DATE NOT NULL,
     quantity INT NOT NULL,
-    PRIMARY KEY (trade_id)
+    PRIMARY KEY (tradeId)
 );
 
 
