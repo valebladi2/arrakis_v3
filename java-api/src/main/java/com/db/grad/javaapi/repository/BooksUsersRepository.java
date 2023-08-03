9c39e9cbcc7a48135b75f6e9726593d67922c831
valebladi <bladinieresjusto@campus.tu-berlin.de>
2023-08-03 13:01:17 +0200

    first story solved, deleted duplicates

--- java-api/src/main/java/com/db/grad/javaapi/repository/BooksUsersRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/BooksUsersRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface BooksUsersRepository extends JpaRepository<Books, String>
 {
-    @Query(nativeQuery = true, value = "select b.bookname, bu.bondholder, b.status, b.issuername, b.isin, b.cusip, b.facevalue, b.bondmaturitydate, b.bondcurrency, b.couponpercent, b.unitprice\n" +
+    @Query(nativeQuery = true, value = "select distinct b.bookname, bu.bondholder, b.status, b.issuername, b.isin, b.cusip, b.facevalue, b.bondmaturitydate, b.bondcurrency, b.couponpercent, b.unitprice\n" +
             "from booksusers bu left join books b \n" +
             "on bu.bookname = b.bookname where bondholder = :bondholder and status = 'active'")
     List<String> getAllBooksUser(String bondholder);
