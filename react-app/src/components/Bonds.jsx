985004d5f666cece1f5a4524c3c0553e320d3ba2
Tudor <tudorciot@gmail.com>
2023-08-03 11:00:43 +0300

    navbar problem resolved

--- react-app/src/components/Bonds.jsx
+++ react-app/src/components/Bonds.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 
 export const Bonds = () => {
   return (
+      <>
+          <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+              <Container>
+                  <Navbar.Brand href="/home"><b>Home</b></Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                      <Nav className="me-auto">
+                          <Nav.Link href="/bonds"><b>Bonds</b></Nav.Link>
+                          <Nav.Link href="/profile"><b>Profile</b></Nav.Link>
+                          <Nav.Link href="/logout"><b>Logout</b></Nav.Link>
+                      </Nav>
+                  </Navbar.Collapse>
+              </Container>
+          </Navbar>
       <div className="card" style={{width: "18rem"}}>
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
@@ -13,6 +30,7 @@ export const Bonds = () => {
           <a href="#" className="card-link">Another link</a>
         </div>
       </div>
+      </>
 
   )
 }
