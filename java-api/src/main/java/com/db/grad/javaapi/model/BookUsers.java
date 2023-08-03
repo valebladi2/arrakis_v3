aff29169e2109002546c04938ce1d6e45b3ef9ad
valebladi <bladinieresjusto@campus.tu-berlin.de>
2023-08-03 10:42:48 +0200

    changes so it runs

--- java-api/src/main/java/com/db/grad/javaapi/model/BookUsers.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/BookUsers.java
@@ -6,18 +6,13 @@ import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
-<<<<<<< HEAD
 @Table(name = "bookusers")
-=======
-@Table(name = "bookUsers")
->>>>>>> origin/main
 public class BookUsers {
     @Id
     private int bookUserId;
     private String bondHolder;
     private String bookName;
 
-<<<<<<< HEAD
     @Column(name = "bookuserid", nullable = false)
     public int getBookUserId() {return bookUserId;}
     public void setBookUserId(int bookUserId) {this.bookUserId = bookUserId;}
@@ -31,18 +26,3 @@ public class BookUsers {
     public void setBookName(String bookName) {this.bookName = bookName;}
 
 }
-=======
-    @Column(name = "bookUserId", nullable = false)
-    public int getBookUserId() {return bookUserId;}
-    public void setBookUserId(int bookUserId) {this.bookUserId = bookUserId;}
-
-    @Column(name = "bondHolder", nullable = false)
-    public String getBondHolder() {return bondHolder;}
-    public void setBondHolder(String bondHolder) {this.bondHolder = bondHolder;}
-
-    @Column(name = "bookName", nullable = false)
-    public String getBookName() {return bookName;}
-    public void setBookName(String bookName) {this.bookName = bookName;}
-
-}
->>>>>>> origin/main
