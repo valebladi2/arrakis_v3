868b8bf649fb201a9571f7181e11e3407ad3736f
Aryan Jain <JainAryan@hotmail.co.uk>
2023-08-03 14:56:09 +0100

    removed Dog everywhere

--- java-api/src/main/java/com/db/grad/javaapi/controller/TradeController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/TradeController.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:3000")
 public class TradeController {
     private TradeHandler TradeService;
 
