c40d2e1b258e613f6a00e7eb2f681daacc37b53f
Tudor <tudorciot@gmail.com>
2023-08-04 17:51:17 +0300

    Added Functionality in Bonds Component

--- java-api/src/main/java/com/db/grad/javaapi/controller/BooksUsersController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BooksUsersController.java
@@ -4,6 +4,7 @@ import com.db.grad.javaapi.model.User;
 import com.db.grad.javaapi.service.BooksUsersService;
 import com.db.grad.javaapi.service.UsersHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class BooksUsersController {
 
     @Autowired
--- react-app/src/components/Bonds.jsx
+++ react-app/src/components/Bonds.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Image from './../images/Deutsche-Bank-Logo.png'
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import {getAllBondsOfAUser} from "../services/bonds-service";
 
 export const Bonds = () => {
 
     const [date, setDate] = useState('12-July-2021');
     const [type, setType] = useState('Sovereign');
     const [isShown, setIsShown] = useState(false);
+    const [bonds, setBonds] = useState([]);
 
     const handleClick = event => {
         setIsShown(true);
@@ -24,6 +26,24 @@ export const Bonds = () => {
         if (shouldLogout)
         navigate(path)
     }
+
+    useEffect(() => {
+        getAllBondsofAUserAPI();
+    }, [])
+
+    const getAllBondsofAUserAPI = () =>
+    {
+        getAllBondsOfAUser()
+            .then(res => {
+                setBonds(res.data);
+            })
+            .catch(err => {
+                setBonds([]);
+                console.log(err);
+            })
+
+    }
+
 
     return (
         < >
@@ -46,8 +66,6 @@ export const Bonds = () => {
                 <div className="card" style={{ width: "18rem", marginLeft: "600px", marginTop: "100px" }}>
                     <div className="card-body" >
                         <h5 className="card-title">Bond</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">Maturity: {date}</h6>
-                        <p className="card-text">Type: {type}</p>
                         <button type="submit" className="btn btn-primary" >Previous</button>
                         <button type="submit" className="btn btn-primary" onClick={handleClick} style={{ marginLeft: "10px" }}>Details</button>
                         <button type="submit" className="btn btn-primary" style={{ marginLeft: "20px" }} >Next</button>
@@ -58,30 +76,113 @@ export const Bonds = () => {
                     <table className="table" style={{ borderStyle: "solid", borderRadius: "20px", borderColor: "gray" }} >
                         <thead>
                             <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
+                                <th scope="col">Book name</th>
+                                <th scope="col">Bond Holder</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Issuer Name</th>
+                                <th scope="col">ISIN</th>
+                                <th scope="col">CUSIP</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Bond Maturity Date</th>
+                                <th scope="col">Bond Currency</th>
+                                <th scope="col">Coupon Percent</th>
+                                <th scope="col">Unit Price</th>
+
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+                                <td>{bonds[0].split(",")[0]}</td>
+                                <td>{bonds[0].split(",")[1]}</td>
+                                <td>{bonds[0].split(",")[2]}</td>
+                                <td>{bonds[0].split(",")[3]}</td>
+                                <td>{bonds[0].split(",")[4]}</td>
+                                <td>{bonds[0].split(",")[5]}</td>
+                                <td>{bonds[0].split(",")[6]}</td>
+                                <td>{bonds[0].split(",")[7]}</td>
+                                <td>{bonds[0].split(",")[8]}</td>
+                                <td>{bonds[0].split(",")[9]}</td>
+                                <td>{bonds[0].split(",")[10]}</td>
                             </tr>
                             <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
+                                <td>{bonds[1].split(",")[0]}</td>
+                                <td>{bonds[1].split(",")[1]}</td>
+                                <td>{bonds[1].split(",")[2]}</td>
+                                <td>{bonds[1].split(",")[3]}</td>
+                                <td>{bonds[1].split(",")[4]}</td>
+                                <td>{bonds[1].split(",")[5]}</td>
+                                <td>{bonds[1].split(",")[6]}</td>
+                                <td>{bonds[1].split(",")[7]}</td>
+                                <td>{bonds[1].split(",")[8]}</td>
+                                <td>{bonds[1].split(",")[9]}</td>
+                                <td>{bonds[1].split(",")[10]}</td>
                             </tr>
                             <tr>
-                                <th scope="row">3</th>
-                                <td colSpan="2">Larry the Bird</td>
-                                <td>@twitter</td>
+                                <td>{bonds[2].split(",")[0]}</td>
+                                <td>{bonds[2].split(",")[1]}</td>
+                                <td>{bonds[2].split(",")[2]}</td>
+                                <td>{bonds[2].split(",")[3]}</td>
+                                <td>{bonds[2].split(",")[4]}</td>
+                                <td>{bonds[2].split(",")[5]}</td>
+                                <td>{bonds[2].split(",")[6]}</td>
+                                <td>{bonds[2].split(",")[7]}</td>
+                                <td>{bonds[2].split(",")[8]}</td>
+                                <td>{bonds[2].split(",")[9]}</td>
+                                <td>{bonds[2].split(",")[10]}</td>
                             </tr>
+                            <tr>
+                                <td>{bonds[3].split(",")[0]}</td>
+                                <td>{bonds[3].split(",")[1]}</td>
+                                <td>{bonds[3].split(",")[2]}</td>
+                                <td>{bonds[3].split(",")[3]}</td>
+                                <td>{bonds[3].split(",")[4]}</td>
+                                <td>{bonds[3].split(",")[5]}</td>
+                                <td>{bonds[3].split(",")[6]}</td>
+                                <td>{bonds[3].split(",")[7]}</td>
+                                <td>{bonds[3].split(",")[8]}</td>
+                                <td>{bonds[3].split(",")[9]}</td>
+                                <td>{bonds[3].split(",")[10]}</td>
+                            </tr>
+                            <tr>
+                                <td>{bonds[4].split(",")[0]}</td>
+                                <td>{bonds[4].split(",")[1]}</td>
+                                <td>{bonds[4].split(",")[2]}</td>
+                                <td>{bonds[4].split(",")[3]}</td>
+                                <td>{bonds[4].split(",")[4]}</td>
+                                <td>{bonds[4].split(",")[5]}</td>
+                                <td>{bonds[4].split(",")[6]}</td>
+                                <td>{bonds[4].split(",")[7]}</td>
+                                <td>{bonds[4].split(",")[8]}</td>
+                                <td>{bonds[4].split(",")[9]}</td>
+                                <td>{bonds[4].split(",")[10]}</td>
+                            </tr>
+                            <tr>
+                                <td>{bonds[5].split(",")[0]}</td>
+                                <td>{bonds[5].split(",")[1]}</td>
+                                <td>{bonds[5].split(",")[2]}</td>
+                                <td>{bonds[5].split(",")[3]}</td>
+                                <td>{bonds[5].split(",")[4]}</td>
+                                <td>{bonds[5].split(",")[5]}</td>
+                                <td>{bonds[5].split(",")[6]}</td>
+                                <td>{bonds[5].split(",")[7]}</td>
+                                <td>{bonds[5].split(",")[8]}</td>
+                                <td>{bonds[5].split(",")[9]}</td>
+                                <td>{bonds[5].split(",")[10]}</td>
+                            </tr>
+                            <tr>
+                                <td>{bonds[6].split(",")[0]}</td>
+                                <td>{bonds[6].split(",")[1]}</td>
+                                <td>{bonds[6].split(",")[2]}</td>
+                                <td>{bonds[6].split(",")[3]}</td>
+                                <td>{bonds[6].split(",")[4]}</td>
+                                <td>{bonds[6].split(",")[5]}</td>
+                                <td>{bonds[6].split(",")[6]}</td>
+                                <td>{bonds[6].split(",")[7]}</td>
+                                <td>{bonds[6].split(",")[8]}</td>
+                                <td>{bonds[6].split(",")[9]}</td>
+                                <td>{bonds[6].split(",")[10]}</td>
+                            </tr>
+
                         </tbody>
                     </table>
                 </>
